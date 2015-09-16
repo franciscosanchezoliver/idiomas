b402cf9d4807d0a685700f0594c9e41b5143ec5e
spuzi <spuzi@spuzi>
2015-09-16 19:40:27 +0200

    njmm

---
+++ preguntas idioma/src/Principal.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicSplitPaneUI.KeyboardDownRightHandler;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PreguntaRespuesta preguntaRespuesta = new PreguntaRespuesta();
+		preguntaRespuesta.cargarPreguntas();
+		preguntaRespuesta.cargarRespuestas();
+
+		while (true == true) {
+			Random r1 = new Random();
+			int valor = r1.nextInt(preguntaRespuesta.getPreguntas().size());
+			System.out.println(valor);
+
+			System.out.print("-" + preguntaRespuesta.getPreguntas().get(valor));
+			try {
+				Scanner scan = new Scanner(System.in);
+				scan.nextLine();
+				System.out
+						.println(preguntaRespuesta.getRespuestas().get(valor));
+				scan.nextLine();
+
+			} catch (Exception e) {
+			}
+
+		}
+	}
+
+}
