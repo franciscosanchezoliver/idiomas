a1c585269c3a4b4e5718c08dd05b6b26e89f0145
NataliaP <NataliaP@NataliaCalabria>
2015-09-17 20:30:18 +0200

    Actualización día 17: Leer y cargar en un HashMap

--- preguntas idioma/src/PreguntaRespuesta.java
+++ preguntas idioma/src/PreguntaRespuesta.java
@@ -1,97 +1,28 @@
-import java.util.ArrayList;
-
+import java.io.*;
+import java.util.*;
 
 public class PreguntaRespuesta {
 
-	ArrayList<String> preguntas = new ArrayList<String>();
-	ArrayList<String> respuestas = new ArrayList<String>();
-	
-	public void cargarPreguntas(){
-		//this is a holy shit. 2do cambio
-		preguntas.add("Este video es para aquellos que os esteis preparando para una entrevista de trabajo en ingles");
-		preguntas.add("He selecionado algunas posibles respuestas");
-		preguntas.add("Te estoy dando algunos consejos utiles");
-		preguntas.add("Espero que te guste el video y lo encuentres util y ten suerte con tu entrevista");
-		preguntas.add("que sabes sobre nuestra compañia?");
-		preguntas.add("eres la mejor persona para este trabajo? Por que?");
-		preguntas.add("Por que quieres este trabajo?");
-		preguntas.add("Dime un poco sobre ti");
-		preguntas.add("Como evaluas el éxito?");
-		preguntas.add("Que es por lo que sientes pasion?");
-		preguntas.add("Dime sobre tu experiencia en el trabajo");
-		preguntas.add("Cuales eran tus reponsabilidades en tu trabajo anterior?");
-		preguntas.add("Describe una experiencia dificil en el trabajo y como lo manejas");
-		preguntas.add("Describa sus metas profesonales a corto y largo plazo");
-		preguntas.add("Describa tu estilo de trabajo");
-		preguntas.add("Prefieres trabajar solo o en equipo?");
-		preguntas.add("Cual es tu mayor virtud");
-		preguntas.add("Cual es tu mayor debilidad?");
-		preguntas.add("Como maneja el estres y la presión?");
-		preguntas.add("Estas dispuesto a trabajar horas extras?");
-		preguntas.add("Cuantos tiempo le gustaría trabjar en esta empresa?");
-		preguntas.add("Por que va a dejar o ha dejado su trabajo anterior");
-		preguntas.add("Que es lo que te gusto o no te gusto de tu trabajo anterior?");
-		preguntas.add("Cual fue su mayor logro o el mayor fracaso?");
-		preguntas.add("Encuentra lo posible sobre la compañia");
-		preguntas.add("Usa vocabulario con el que te encuentres agusto");
-		preguntas.add("Practica entrevistando en frete de un espejo o grabate a ti mismo");
-		preguntas.add("No respondas con 'si' o 'no', elabora tus respuestas, provee ejemplos y si es aplicable");
-		preguntas.add("No cometas el error de decirle al que entrevista que quieres decirle");
-		preguntas.add("Manten las respuestas a las preguntas de la entrevista  cortas y al punto");
-		preguntas.add("No memorices las respuestas");
-		preguntas.add("Cuando prepares tus respuestas de la entrevista de trabajo, escribe algunas palabras clave, y no frases");
+	Map<String, String> entrevista = new HashMap<>();
+
+	public void cargarPreguntas(String archivoEntrada){
+		String cadena;
+		StringTokenizer tokenizer=null;
+		BufferedReader in = null;
+		String pregunta = "", respuesta= "";
 		
-	}
+		try{
+			in = new BufferedReader(new FileReader(archivoEntrada));
+		    while((cadena = in.readLine()) != null){
+		    	tokenizer = new StringTokenizer(cadena, "-");
+		    	pregunta = tokenizer.nextToken();
+		    	respuesta = tokenizer.nextToken();
+		    	this.entrevista.put(pregunta, respuesta);
+		    }
+		    in.close();
+		}catch (IOException e){
+			System.out.println(e);
+		}
+	} //Fin cargarPreguntas
 	
-	public void cargarRespuestas(){
-		respuestas.add("This video is for those of you who are preparing for a job interview in English.");
-		respuestas.add("I have selected some possible answers");
-		respuestas.add("I’m giving you some useful tips");
-		respuestas.add("I hope you enjoy the video and find it useful and have good luck with your interview");
-		respuestas.add("What do you know about our company?");
-		respuestas.add("Are you the best person for this job? why?");
-		respuestas.add("Why do you want this job?");
-		respuestas.add("Tell me a little about yourself");
-		respuestas.add("How do you evaluate success?");
-		respuestas.add("What are you passionate about?");
-		respuestas.add("Tell me about your work experience");
-		respuestas.add("What were your responsibilities in your previous job?");
-		respuestas.add("Describe a difficult experience at work and how you handled it");
-		respuestas.add("Describe your short term and long term career goals");
-		respuestas.add("Describe your work style");
-		respuestas.add("Do you prefer to work alone or on a team?");
-		respuestas.add("What is your greatest strength?");
-		respuestas.add("What is your greatest weakness?");
-		respuestas.add("How do you handle stress and pressure?");
-		respuestas.add("How do you feel about working overtime?");
-		respuestas.add("How long do you expect to work for this company?");
-		respuestas.add("Why are you leaving or have left your previous job?");
-		respuestas.add("What did you like or dislike about your previous job?");
-		respuestas.add("What was your biggest accomplishment or failure?");
-		respuestas.add("Find out as much as possible about the company");
-		respuestas.add("Use vocabulary you're comfortable with");
-		respuestas.add("Practice interviewing in front of a mirror or record yourself");
-		respuestas.add("Don't answer with just a 'yes' or 'no', elaborate your answer, provide examples if applicable");
-		respuestas.add("Do not make the mistake to telling the interviewer what you want to tell him");
-		respuestas.add("Keep your answers to interview questions short and to the point");
-		respuestas.add("Don't memorize your answers");
-		respuestas.add("When you prepare your job interview answers, write down some keywords, and not phrases");
-	}
-
-	public ArrayList<String> getPreguntas() {
-		return preguntas;
-	}
-
-	public void setPreguntas(ArrayList<String> preguntas) {
-		this.preguntas = preguntas;
-	}
-
-	public ArrayList<String> getRespuestas() {
-		return respuestas;
-	}
-
-	public void setRespuestas(ArrayList<String> respuestas) {
-		this.respuestas = respuestas;
-	}
-	
-}
+} // Fin clase
--- preguntas idioma/src/Principal.java
+++ preguntas idioma/src/Principal.java
@@ -1,30 +1,18 @@
+import java.io.File;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Principal {
 
+	private static String directorioEntrada = System.getProperty("user.dir") + File.separator + "src"
+						+ File.separator + "PregResp";
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PreguntaRespuesta preguntaRespuesta = new PreguntaRespuesta();
-		preguntaRespuesta.cargarPreguntas();
-		preguntaRespuesta.cargarRespuestas();
+		preguntaRespuesta.cargarPreguntas(directorioEntrada);
 
-		while (true == true) {
-			Random r1 = new Random();
-			int valor = r1.nextInt(preguntaRespuesta.getPreguntas().size());
-			System.out.println(valor);
-
-			System.out.print("-" + preguntaRespuesta.getPreguntas().get(valor));
-			try {
-				Scanner scan = new Scanner(System.in);
-				scan.nextLine();
-				System.out.println(preguntaRespuesta.getRespuestas().get(valor));
-				scan.nextLine();
-			} catch (Exception e) {
-				System.out.println("Francisco le come el chichi a Natalia");
-			}
-
-		}
 	}
 
 }
